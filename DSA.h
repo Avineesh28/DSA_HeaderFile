9b1ca052af8a33a241cefb9f8b924d7177d2441b
Avineesh Sathyakumar <avineeshsathyakumar@gmail.com>
2021-12-05 22:58:56 +0530

    Update DSA.h

--- DSA.h
+++ DSA.h
@@ -88,15 +88,107 @@ void addNodeP(int p)
 		temp->next=temp;	
 	c++;
 }
+void delNodeB()
+{
+	struct node *temp=head;
+	if(head!=NULL)
+	{
+		while(temp->next!=head)
+			temp=temp->next;
+		if(temp==head)
+			head=NULL;
+		else
+		{
+			temp->next=head->next;
+			head=head->next;
+		}
+	}
+	else
+		printf("List Empty\n");
+	c--;
+				
+}
+void delNodeE()
+{
+	if(head!=NULL)
+	{
+		struct node *temp1=head;	
+		while(temp1->next->next!=head)
+			temp1=temp1->next;
+		temp1->next=head;
+	}
+	else if(c==1)
+	{
+		head=head->next;
+	}
+	else
+		printf("List Empty\n");
+		c--;
+}
+void delNodeP(int p)
+{
+	int c=1;
+	struct node *temp=head;
+	while(c!=p-1)
+	{
+		temp=temp->next;
+		c++;
+	}
+	c=1;
+	temp->next=temp->next->next;
+	c--;
+}
+void search(int item)
+{
+    struct node *ptr;
+    int i=0,flag=1;
+    ptr = head;
+    if(ptr == NULL)
+    {
+        printf("\nList is Empty\n");
+    }
+    else
+    {
+        if(head->info == item)
+        {
+        	printf("Item found at location %d\n",i+1);
+        	flag=0;
+        }
+        else
+        {
+        	while (ptr->next != head)
+        	{
+	            if(ptr->info == item)
+	            {
+	                printf("Item found at location %d \n",i+1);
+	                flag=0;
+	                break;
+	            }
+	            else
+	            {
+	                flag=1;
+	            }
+	            i++;
+	            ptr = ptr -> next;
+	        }
+	    }
+     	if(flag != 0)
+       	    printf("Item not found\n");
+    }
+}
 void display()
 {
-	printf("Circular Linked List:\n\t==>");
-	struct node *temp=head;
-	while(temp->next!=head)
+	if(head!=NULL)
 	{
-		
-		printf("%d-->",temp->info);
-		temp=temp->next;
+		printf("Circular Linked List:\n\t==>");
+		struct node *temp=head;
+		while(temp->next!=head)
+		{
+			printf("%d-->",temp->info);
+			temp=temp->next;
+		}
+		printf("%d==>\n",temp->info);
 	}
-	printf("%d==>\n",temp->info);
+	else
+		printf("List is Empty");
 }
